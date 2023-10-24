85600b0eb7b4509eab9b1888a37cd2d4e0d20a81
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 14:34:17 +0300

    commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> data = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(": ");
+                if (parts.length == 2) {
+                    data.put(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file: " + file.getAbsolutePath(), e);
+        }
+
+        String name = data.get("Name");
+        int age = Integer.parseInt(data.get("Age"));
+        String email = data.get("Email");
+        long phone = Long.parseLong(data.get("Phone"));
+
+        return new Profile(name, age, email, phone);
     }
 }
